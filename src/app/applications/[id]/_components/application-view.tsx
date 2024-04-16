3c2a74aee8111787ea7b1126b81460b5a3d1d4a6
Amine Tayani <amine.tayanipro@gmail.com>
2024-04-16 17:01:17 +0100

    fix: description not updating when editing

--- src/app/applications/[id]/_components/application-view.tsx
+++ src/app/applications/[id]/_components/application-view.tsx
@@ -1,44 +1,17 @@
 'use client';
 
-import { EditorContent, useEditor } from '@tiptap/react';
-import StarterKit from '@tiptap/starter-kit';
-import Heading from '@tiptap/extension-heading';
-import Paragraph from '@tiptap/extension-paragraph';
-import Text from '@tiptap/extension-text';
 import { EmptyPlaceholder } from '@/components/ui/empty-placeholder';
 import { Application } from '@/lib/db/types';
 import { EditApplicationButton } from '@/components/dashboard/applications/edit-application-button';
+import { DescriptionViewer } from '@/components/ui/description-viewer';
 
 export function ApplicationView({ application }: { application: Application }) {
-  const editor = useEditor({
-    editable: false,
-    content: `${application.description}`,
-    editorProps: {
-      attributes: {
-        class:
-          'text-sm text-neutral-200 prose max-w-none custom-scrollable-element h-[400px] overflow-y-auto mb-8',
-      },
-    },
-    extensions: [
-      StarterKit,
-      Paragraph,
-      Text,
-      Heading.configure({
-        levels: [1, 2, 3, 4],
-      }),
-    ],
-  });
-
-  if (!editor) {
-    return null;
-  }
-
   if (!application) {
     return (
       <div className='col-span-4 flex h-full flex-col'>
         <EmptyPlaceholder className='my-4 '>
           <EmptyPlaceholder.Description>
-            No content
+            No content has been added to this application yet.
           </EmptyPlaceholder.Description>
         </EmptyPlaceholder>
       </div>
@@ -73,7 +46,7 @@ export function ApplicationView({ application }: { application: Application }) {
           <h1 className='mb-4 inline-block text-xl font-bold tracking-tight text-neutral-100'>
             Job description
           </h1>
-          <EditorContent editor={editor} />
+          <DescriptionViewer content={application.description} />
         </div>
       </div>
     </div>
