8c9ef2f9cd359e09368e153aa02ccb875704762a
Amine Tayani <amine.tayanipro@gmail.com>
2024-04-29 16:52:17 +0100

    fix: show empty placeholder when there is no apps

--- src/components/dashboard/application-content.tsx
+++ src/components/dashboard/application-content.tsx
@@ -5,6 +5,7 @@ import { Application } from '@/lib/db/types';
 import { ApplicationsContentHeader } from './applications-content-header';
 import { GridView } from './applications/grid-view';
 import { TableView } from './applications/table-view';
+import { EmptyPlaceholder } from '../ui/empty-placeholder';
 
 interface ApplicationsContentProps {
   applications: Application[];
@@ -17,15 +18,18 @@ export function ApplicationsContent({
   const mode = params.get('mode');
 
   return (
-    <>
-      <div className='flex-1 px-6'>
-        <ApplicationsContentHeader />
-        {mode === 'table' ? (
-          <TableView applications={applications} />
-        ) : (
-          <GridView applications={applications} />
-        )}
-      </div>
-    </>
+    <div className='flex-1 px-6'>
+      <ApplicationsContentHeader />
+
+      {applications.length === 0 ? (
+        <EmptyPlaceholder className='mt-4'>
+          No applications found. Try adding a new one.
+        </EmptyPlaceholder>
+      ) : mode === 'table' ? (
+        <TableView applications={applications} />
+      ) : (
+        <GridView applications={applications} />
+      )}
+    </div>
   );
 }
