e6a67acc14a911e01c01e4492a16742fdcaba1d1
amine-tayani <amine.tayanipro@gmail.com>
2025-01-03 22:00:07 +0100

    feat: add new pages to dashboard

---
+++ src/app/dashboard/analytics/page.tsx
@@ -0,0 +1,9 @@
+export default function AnalyticsPage() {
+  return (
+    <div className='mx-4 mt-4 flex max-w-3xl flex-col space-y-6'>
+      <div>
+        <h1 className='text-7xl font-semibold tracking-wide'>Analytics</h1>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/dashboard/inbox/page.tsx
@@ -0,0 +1,9 @@
+export default function InboxPage() {
+  return (
+    <div className='mx-4 mt-4 flex max-w-3xl flex-col space-y-6'>
+      <div>
+        <h1 className='text-7xl font-semibold tracking-wide'>Inbox</h1>
+      </div>
+    </div>
+  );
+}
